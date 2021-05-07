4fd8e8bd4f1bb8bebf8521539e967342ea8207a8
ErikKarlgren <erik.rivas8@gmail.com>
2021-05-08 00:14:10 +0200

    Server now accepts several concurrent Clients

--- Servidor/src/ucm/erikkarl/server/ServerMain.java
+++ Servidor/src/ucm/erikkarl/server/ServerMain.java
@@ -5,6 +5,7 @@ import ucm.erikkarl.common.ServerInformation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
@@ -14,13 +15,29 @@ public class ServerMain {
 
         try (var serverSocket = new ServerSocket(ServerInformation.PORT))
         {
-            var socket = serverSocket.accept();
+            while (true)
+            {
+                var socket = serverSocket.accept();
+                System.out.println("Connection established with " + socket.toString());
+                new Thread(() -> chatWithClient(socket)).start();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private static void chatWithClient(Socket socket) {
+        try
+        {
             var inputFromClient = new Scanner(socket.getInputStream(), StandardCharsets.UTF_8);
             var outputToClient = new PrintWriter(socket.getOutputStream(), true, StandardCharsets.UTF_8);
 
             outputToClient.println("Welcome to my Minecraft Server! UwU");
             outputToClient.println("Now, tell me your name: ");
             var name = inputFromClient.nextLine();
+            System.out.println("< "+socket + " > Client's name is " + name);
             outputToClient.println("Welcome " + name + "!");
         }
         catch (IOException e)
